a6a8b5c4b098c8bce8d911f573d3b40c6a8a9752
ysjeondev <hpc9909@gmail.com>
2025-08-10 23:57:06 +0900

    book_80p까지

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url,true).query;
     console.log(queryData.id);
+    var title = queryData.id;  // 요청받은 쿼리 스트링 중 id 값을 title 변수에 할당
+
     if(_url == '/') {
-        _url = '/index.html';
+        title = 'Welcome';
     }
     if(_url == '/favicon.ico') {
         return response.writeHead(404);
@@ -17,17 +19,17 @@ var app = http.createServer(function(request, response) {
     var template = `
         <html>
             <head>
-                <title>WEB1 - HTML</title>
+                <title>WEB1 - ${title}</title>
                 <meta charset="utf-8">
             </head>
             <body>
-                <h1><a href="index.html">WEB</a></h1>
+                <h1><a href="/">WEB</a></h1>
                 <ol>
-                    <li><a href="1.html">HTML</a></li>
-                    <li><a href="2.html">CSS</a></li>
-                    <li><a href="3.html">JavaScript</a></li>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=css">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
                 </ol>
-                <h2>HTML</h2>
+                <h2>${queryData.id}</h2>
                 <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
                 <img src="coding.jpg" width="100%">
                 </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
@@ -36,7 +38,7 @@ var app = http.createServer(function(request, response) {
         </html>
         `;
         
-    response.end(queryData.id)
+    response.end(template)
     //response.end(fs.readFileSync(__dirname + _url));
 });
 app.listen(3000);
